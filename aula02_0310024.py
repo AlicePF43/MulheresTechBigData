e72461ac81f19edb4da608384f77f29a5feb6d19
AlicePF43 <alicedpf43@gmail.com>
2024-10-04 21:39:29 -0300

    Add files via upload

---
+++ aula02_0310024.py
@@ -0,0 +1,77 @@
+print('Sorvete')
+#Isso é um comentário tipo texto (string)
+'''
+Isso também é um cometário 
+
+'''
+print(123) #int
+print(1245,2154) #decimal
+print(2124.10)  #float
+print(True) #boolean
+print(False) 
+print(None) #none, null, NaN
+print(['string']) #estrutura lista
+print(('string')) #tupla
+print ({'string': 'texto'}) #dicionário
+
+#IDENTAÇÃO#---04/10/2024
+#Condicionais#
+# idade=23
+# idade1=20
+# idade2=17
+# if idade>=18: 
+#     print("Você é maior de idade. Pode entrar.")
+# else:
+#     print("Você é menor de idade. Vai estudar.")
+
+# temperatura=36
+
+# if temperatura<37.5:#usar dois pontos ":" sempre 
+#     print("Você não está com febre.")
+# elif temperatura==37.5:
+#     print("Você está febril.")
+# else:
+#     print("Você está febril")
+
+# pessoa menor de idade, pessoa está entre a faixa 16 a 18 anos, maior de idade
+
+# ingresso= 20
+
+# if  ingresso >18:
+#     print("Seus ingressos estão disponíveis para retirada.")
+# elif ingresso >=16 and ingresso <=18:
+#     print("Retire somente com responsável.")
+# else: 
+#     print("Você não pode retirar seu ingresso")
+
+
+# Sistema de comparação
+
+###COMPARATIVOS NO PRINT###
+
+fruta1='jambo'
+fruta2='maçã'
+fruta3='uva'
+fruta4='maçã'
+num1=67
+num2=12
+num3=12.0
+num4=67.1
+
+print(fruta1==fruta3)
+fruta1=fruta3
+print(fruta1)
+
+print(fruta1==fruta4)
+print(fruta2==fruta4)
+print(fruta3==fruta4)
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num2==num3)
+print(num1!=num4)
+
+print(float(num2))
+
+
